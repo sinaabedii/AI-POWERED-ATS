5d28c9f925a0b9f0b3690180b81d5f426d9915bb
Sina Abedi <sinaabediofficial@gmail.com>
2025-04-28 12:08:07 +0330

    fix : edit problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-// src/App.tsx
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { AuthProvider } from './contexts/AuthContext';
-
-// Auth Pages
-
-// Public Pages
 import { HomePage } from './pages/public/Home';
 import { JobsPage } from './pages/public/JobsPage';
 import { AboutPage } from './pages/public/AboutPage';
 
-// Dashboard Pages
 import { DashboardHome } from './pages/dashboard/Dashboard';
 import { JobsManagement } from './pages/dashboard/JobsManagement';
 import { ApplicantsManagement } from './pages/dashboard/ApplicantsManagement';
@@ -27,17 +21,12 @@ function App() {
       <AuthProvider>
         <Router>
           <Routes>
-            {/* Public Routes */}
             <Route path="/" element={<HomePage />} />
             <Route path="/jobs" element={<JobsPage />} />
             <Route path="/jobs/:id" element={<JobDetailPage />} />
             <Route path="/about" element={<AboutPage />} />
-            
-            {/* Auth Routes */}
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
-            
-            {/* Protected Dashboard Routes */}
             <Route path="/dashboard" element={
               <ProtectedRoute>
                 <DashboardHome />
@@ -63,8 +52,6 @@ function App() {
                 <SettingsPage />
               </ProtectedRoute>
             } />
-            
-            {/* Catch all - redirect to home */}
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </Router>
